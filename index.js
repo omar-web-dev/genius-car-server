a91a94e769b5b5a518505ad5135c663d533e032c
omarwebdeve <omarf977865@gmail.com>
2022-11-05 14:16:05 +0600

    delete data

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const cors = require("cors")
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const { JsonWebTokenError } = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
+
+// random bytes
+// require('crypto').randomBytes(64).toString('hex')
 
 const port = process.env.PORT || 5000
 
 require('dotenv').config()
-console.log(process.env.DB_PASSWORD)
-console.log(process.env.DB_USER)
 
 app.use(cors())
 app.use(express.json())
@@ -18,16 +21,81 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uadalh8.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        // collection 
+        // genius car database name
+        // service DB name 
+        const serviceCollection = client.db('geniusCar').collection('services')
+        const orderCollection = client.db('geniusCar').collection('orders')
+
+        app.get('/services', async (req, res) => {
+            // query
+            const query = {}
+            // find data form your collection
+            const cursor = serviceCollection.find(query)
+            // convert to Array onek gula data find korar somoi array te convert korte hobe
+            const services = await cursor.toArray()
+            // request send 
+            res.send(services)
+
+        })
+
+        // create user token
+        app.post('/jwt', (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn : '1hr'})
+            res.send({token})
+            console.log(user)
+            
+        })
+
+        // get single collection
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await serviceCollection.findOne(query)
+            res.send(service)
+        })
+
+        // delete api
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const order = await orderCollection.deleteOne(query)
+            res.send(order)
+        })
+
+        // order api
+        // get all collection
+        app.get('/orders', async (req, res) => {
+            // find out email 
+            let query = {}
+            if(req.query.email){
+                query = {
+                    email : req.query.email
+                }
+            }
+                const cursor = orderCollection.find(query)
+                const order = await cursor.toArray()
+                res.send(order)
+            })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order)
+            res.send(result)
+        })
+    }
+    finally { }
+}
+
+run()
 
 
 app.listen(port, () => {
     console.log('server is ', port)
-} )
+})
 
